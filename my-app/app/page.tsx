8bb7bf18fed6c03ae056c1fc3cf1dec13f41b6b3
stino-x <107426247+stino-x@users.noreply.github.com>
2024-09-03 22:28:02 +0200

    build(deps): update page.tsx, package-lock.json and package.json

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,19 +5,11 @@ import { useRouter } from 'next/navigation'
 
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleLogout = async () => {
-    const loggedOut = await logoutAccount();
-    router.push('/signin');
-    console.log('Logged out finish');
-  };
+  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <button type="button" onClick={handleLogout}>
-          Logout
-        </button>
+        DASHBOARD
     </main>
   );
 }
