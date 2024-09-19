ccd28e1eeac893eb076581935df2cee3084558c6
Booze33 <tebetisloh@gmail.com>
2024-09-19 15:31:54 +0200

    update table

--- my-app/components/table/columns.tsx
+++ my-app/components/table/columns.tsx
@@ -13,17 +13,39 @@ export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "task",
     header: "Task",
+    cell: ({ row }) => (
+      <div className="text-left font-semibold">{row.original.task}</div>
+    ),
   },
   {
     accessorKey: "due_date",
     header: "Due date",
+    cell: ({ row }) => (
+      <div className="text-center text-blue-600">{row.original.due_date}</div>
+    ),
   },
   {
     accessorKey: "assignee",
     header: "Assignee",
+    cell: ({ row }) => (
+      <div className="text-left px-2 text-gray-800">{row.original.assignee}</div>
+    ),
   },
   {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => (
+      <div
+        className={`text-center font-bold ${
+          row.original.status === "Completed"
+            ? "text-green-500"
+            : row.original.status === "In Progress"
+            ? "text-yellow-500"
+            : "text-red-500"
+        }`}
+      >
+        {row.original.status}
+      </div>
+    ),
   },
 ]
--- my-app/components/table/data-table.tsx
+++ my-app/components/table/data-table.tsx
@@ -34,12 +34,12 @@ export function DataTable<TData, TValue>({
   return (
     <div className="rounded-md border">
       <Table>
-        <TableHeader>
+        <TableHeader className="bg-[#f9fafb]">
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
                 return (
-                  <TableHead key={header.id}>
+                  <TableHead key={header.id} className="px-2">
                     {header.isPlaceholder
                       ? null
                       : flexRender(
@@ -60,7 +60,7 @@ export function DataTable<TData, TValue>({
                 data-state={row.getIsSelected() && "selected"}
               >
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
+                  <TableCell key={cell.id} className="px-2">
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
