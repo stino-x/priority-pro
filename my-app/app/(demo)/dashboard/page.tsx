5d520d104ac75394a049b9248b1eb688073acb62
Booze33 <tebetisloh@gmail.com>
2024-10-14 00:17:06 +0200

    update dashboard

--- my-app/app/(demo)/dashboard/page.tsx
+++ my-app/app/(demo)/dashboard/page.tsx
@@ -6,10 +6,16 @@ import { ContentLayout } from '@/components/admin-panel/content-layout';
 import { getLoggedInUser } from '@/lib/actions/user.action';
 import { getDailyTasks } from '@/lib/actions/task.action';
 import { Task } from "@/lib/interfaces/interface";
+import { MessageCircleMore } from 'lucide-react';
+
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashboardPage() {
   const [currentUser, setCurrentUser] = useState<any>(null);
   const [task, setTask] = useState<Task[]>([]);
+
+  const completed = [];
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -44,6 +50,16 @@ export default function DashboardPage() {
     fetchTask();
   }, []);
 
+  task.map((item) => {
+    if (item.completed) {
+      completed.push(item);
+    }
+  })
+
+  const result = (completed.length / task.length) * 100;
+
+  const percentage = Math.floor(result);
+
   return (
     <ContentLayout title="Dashboard">
       <div>
@@ -54,8 +70,19 @@ export default function DashboardPage() {
         <div className="flex flex-col items-center w-full mt-8">
           <h1>All Tasks</h1>
           <div className="flex flex-row w-[100%] justify-around">
-            <div className="w-[35vw] h-[7rem] bg-[#D3E0EA] dark:bg-[#404258] rounded-xl">date</div>
-            <div className="w-[35vw] h-[7rem] bg-[#D3E0EA] dark:bg-[#404258] rounded-xl">notification</div>
+            <div className="w-[35vw] h-[7rem] bg-[#D3E0EA] dark:bg-[#404258] rounded-xl flex flex-col justify-center items-center">
+              <MessageCircleMore className="w-[3rem] h-[3rem] text-[#ffadff]" />
+              <h2 className="font-semibold text-lg text-zinc-500">Chats</h2>
+            </div>
+            <div className="w-[35vw] h-[7rem] bg-[#D3E0EA] dark:bg-[#404258] rounded-xl flex flex-col justify-center items-center">
+              <CircularProgressbar
+                minValue={0}
+                maxValue={100}
+                value={percentage}
+                text={`${percentage}%`}
+                className="w-[4rem] h-[4rem] text-xs"
+              />
+            </div>
           </div>
           <div className="flex flex-col w-[100%] justify-around mt-4">
             {task.length > 0 ? (
@@ -64,7 +91,7 @@ export default function DashboardPage() {
                   <h3 className={`text-center w-[5rem] rounded-2xl font-semibold text-xs text-[#26262f] dark:text-[#d9d9d9] ${t.completed ? "bg-green-100" : "bg-red-400"}`}>
                   {t.completed ? "Completed" : "Not Started"}
                   </h3>
-                  <h2>{t.title}</h2>
+                  <h2 className="font-semibold text-lg">{t.title}</h2>
                   <div className="flex flex-row justify-between mt-2">
                     <h4 className="font-semibold text-xs text-zinc-500">{t.verified_at ? new Date(t.verified_at).toLocaleDateString() : 'N/A'}</h4>
                     <h4 className={`pb-0 text-center w-[5rem] rounded-2xl font-semibold text-xs text-[#26262f] dark:text-[#26262f] ${t.is_verified ? "bg-green-100" : "bg-gray-100"}`}>
