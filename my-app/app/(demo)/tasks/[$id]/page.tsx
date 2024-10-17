30807f7c9f24fa1ce6f9d1d42cab8a77478131ad
Booze33 <tebetisloh@gmail.com>
2024-10-17 17:41:12 +0200

    update task page

--- my-app/app/(demo)/tasks/[$id]/page.tsx
+++ my-app/app/(demo)/tasks/[$id]/page.tsx
@@ -12,10 +12,8 @@ import {
   BreadcrumbSeparator
 } from "@/components/ui/breadcrumb";
 import { getTaskById } from "@/lib/actions/task.action";
-// import { Payment, columns } from "@/components/table/columns"
-// import { getData } from "./data"
-// import { getLoggedInUser } from "@/lib/actions/user.action";
-// import Tasktabs from "@/components/task-components/Tasktabs";
+import { Dot } from 'lucide-react';
+import { CiCalendarDate } from "react-icons/ci";
 
 export default async function TaskPage({ params: { $id } }: { params: { $id: string } }) {
   const fetchTask = await getTaskById($id);
@@ -35,12 +33,54 @@ export default async function TaskPage({ params: { $id } }: { params: { $id: str
           <BreadcrumbSeparator />
           <BreadcrumbItem>
             <BreadcrumbPage>
-              Task Page
+              Task detail
             </BreadcrumbPage>
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <h1>{fetchTask.title}</h1>
+      <section>
+        <div>
+          <ul className="flex flex-col list-disc">
+            <li className="flex flex-row my-2">
+              <Dot className="w-8 h-8 mr-1" />
+              <div className="flex flex-col">
+                <h2 className="font-semibold text-xs text-zinc-900">Task Created</h2>
+                <p className="font-semibold text-xs text-zinc-500">{task.created_at}</p>
+              </div>
+            </li>
+            <li className="flex flex-row my-2">
+              <Dot className="w-8 h-8 mr-1" />
+              <div className="flex flex-col">
+                <h2 className="font-semibold text-xs text-zinc-900">Task Verified</h2>
+                <p className="font-semibold text-xs text-zinc-500">{task.verified_at ? new Date(task.verified_at).toLocaleDateString() : 'N/A'}</p>
+              </div>
+              
+            </li>
+            <li className="flex flex-row my-2">
+              <Dot className="w-8 h-8 mr-1" />
+              <div className="flex flex-col">
+                <h2 className="font-semibold text-xs text-zinc-900">Task Completed</h2>
+                <p className="font-semibold text-xs text-zinc-500">{task.completed_at ? new Date(task.verified_at).toLocaleDateString() : 'N/A'}</p>
+
+              </div>
+            </li>
+          </ul>
+          <p className="font-semibold text-xs text-zinc-500 mt-2">Due date: {task.due_date}</p>
+
+        </div>
+        <div className="mt-4">
+          <h1 className="font-semibold text-lg text-zinc-900">Details</h1>
+          <div className="flex flex-row mt-4">
+            <div className="bg-gray-100 w-[1.5rem] h-[1.5rem] rounded mr-4 items-center justify-center">
+              <CiCalendarDate />
+            </div>
+            <div className="flex flex-col">
+              <h2 className="font-semibold text-xs text-zinc-900">{task.title}</h2>
+              <p className="font-semibold text-xs text-zinc-500">{task.description}</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </ContentLayout>
   );
 }
