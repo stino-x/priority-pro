ec76131cdd67ddf714e9c248bc6d02d1466aeadc
stino-x <107426247+stino-x@users.noreply.github.com>
2024-10-27 02:43:43 +0200

    Add PriorityBoard component to display tasks in order of urgency

---
+++ my-app/app/(demo)/priority-board/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PriorityBoard: React.FC = () => {
+    return (
+        <div>
+            <h1>My Priority Board</h1>
+            <p>This shows all the tasks assigned to you for this week in order of urgency</p>
+            {/* CARD */}
+        </div>
+    );
+};
+
+export default PriorityBoard;
