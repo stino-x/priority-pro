398e3660daac803c5befaf9c2f478abdd4d1cc65
stino-x <107426247+stino-x@users.noreply.github.com>
2024-11-27 10:39:26 +0100

    error logging

--- my-app/components/authcomponents/AuthForm.tsx
+++ my-app/components/authcomponents/AuthForm.tsx
@@ -3,20 +3,20 @@
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
-import { Form } from "@/components/ui/form"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
 import CustomInput from './CustomInput';
-import { authFormSchema } from '@/lib/utils';
+import {AuthFormProps, AuthFormSchema, signinSchema, registerSchema } from '@/lib/utils';
 import { register, signIn, handleOAuthLogin, getLoggedInUser } from '@/lib/actions/user.action';
 import useGetRestaurants from "@/lib/hooks/useGetRestaurants";
 import { useToast } from "@/hooks/use-toast";
 
-const AuthForm = ({ type }: { type: string }) => {
+const AuthForm = ({ type }: AuthFormProps) => {
   const router = useRouter();
-  const {toast} = useToast()
+  const { toast } = useToast();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [user, setUser] = useState(null);
@@ -38,25 +38,33 @@ const AuthForm = ({ type }: { type: string }) => {
     fetchUser();
   }, [router]);
 
-  const formSchema = authFormSchema(type)
+  // Select schema and default values based on `type`
+  const formSchema = type === 'signin' ? signinSchema : registerSchema;
+  const defaultValues: Partial<AuthFormSchema> =
+    type === 'register'
+      ? {
+          name: "",
+          email: "",
+          password: "",
+          restaurant: "",
+          picture: null,
+        }
+      : {
+          email: "",
+          password: "",
+        };
 
-  const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm<AuthFormSchema>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-      restaurant: "",
-      picture: null,
-    },
-  })
+    defaultValues,
+  });
 
-  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+  const onSubmit = async (data: AuthFormSchema) => {
     setIsLoading(true);
     setError(null);
 
     try {
-      if(type === 'register') {
+      if (type === 'register' && 'picture' in data) {
         const file = (data.picture as FileList)?.[0] || null;
 
         let pictureBase64 = null;
@@ -72,37 +80,35 @@ const AuthForm = ({ type }: { type: string }) => {
           name: data.name!,
           email: data.email,
           password: data.password,
-          restaurant: data.restaurant,
+          restaurant: data.restaurant!,
           picture: pictureBase64 as string,
-        }
-
-        console.log(userData)
+        };
 
         const newUser = await register(userData);
-        if(newUser) router.push('/dashboard');
+        if (newUser) router.push('/dashboard');
       }
 
-      if(type === 'signin') {
+      if (type === 'signin') {
         const userData = {
           email: data.email,
           password: data.password,
-        }
+        };
 
         const signInResult = await signIn(userData);
-        if(signInResult) router.push('/dashboard');
+        if (signInResult) router.push('/dashboard');
       }
     } catch (error) {
       console.error(`Error during ${type}:`, error);
       toast({
         title: `Error during ${type}:`,
         description: "Please try again.",
-        variant: "destructive", // This will style the toast as an error message
+        variant: "destructive",
       });
       setError(error instanceof Error ? error.message : 'An unexpected error occurred');
     } finally {
       setIsLoading(false);
     }
-  }
+  };
 
   const handleGoogleLogin = async () => {
     try {
@@ -111,24 +117,27 @@ const AuthForm = ({ type }: { type: string }) => {
       console.error('Error during Google login:', error);
       setError(error instanceof Error ? error.message : 'An unexpected error occurred during Google login');
     }
-  }
+  };
 
   return (
     <main className="w-[100vw] h-[100vh] bg-slate-100 flex flex-col justify-center items-center">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-[80vw] sm:w-[40vw] rounded shadow-md shadow-slate-950 p-12 bg-[#fff]">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-[80vw] sm:w-[40vw] rounded shadow-md shadow-slate-950 p-12 bg-[#fff]"
+        >
           {type === 'register' && (
-            <CustomInput control={form.control} name='name' label="Full Name" placeholder='Enter your full name' />
+            <CustomInput control={form.control} name="name" label="Full Name" placeholder="Enter your full name" />
           )}
 
-          <CustomInput control={form.control} name='email' label="Email" placeholder='Enter your email' />
-          <CustomInput control={form.control} name='password' label="Password" placeholder='Enter your password' />
+          <CustomInput control={form.control} name="email" label="Email" placeholder="Enter your email" />
+          <CustomInput control={form.control} name="password" label="Password" placeholder="Enter your password" />
 
           {type === 'register' && (
             <CustomInput
               control={form.control}
               name="restaurant"
-              label="your restaurant name"
+              label="Your Restaurant Name"
               isDropdown
               options={restaurants.map((restaurant: any) => ({
                 label: restaurant.name,
@@ -136,10 +145,6 @@ const AuthForm = ({ type }: { type: string }) => {
               }))}
             />
           )}
-
-
-          {/* IMAGE UPLAOD */}
-
 
           {type === 'register' && (
             <CustomInput
@@ -153,8 +158,8 @@ const AuthForm = ({ type }: { type: string }) => {
 
           {error && <p className="text-red-500 mt-2">{error}</p>}
 
-          <Button type="submit" className="mt-4 w-full" disabled={isLoading}>
-            {isLoading ? 'Loading...' : (type === 'signin' ? 'Sign In' : 'Register')}
+          <Button type="submit" className="mt-4 w-full">
+            {isLoading ? 'Loading...' : type === 'signin' ? 'Sign In' : 'Register'}
           </Button>
         </form>
       </Form>
@@ -165,9 +170,7 @@ const AuthForm = ({ type }: { type: string }) => {
 
       <footer className="flex justify-center gap-1 mt-4">
         <p className="text-14 font-normal text-gray-600">
-          {type === 'signin'
-          ? "Don't have an account?"
-          : "Already have an account?"}
+          {type === 'signin' ? "Don't have an account?" : "Already have an account?"}
         </p>
         <Link href={type === 'signin' ? '/signup' : '/signin'} className="form-link">
           {type === 'signin' ? 'Register' : 'Sign in'}
